ad824fcc3e1fc369c9d51b8fea3afb0f4d08659a
HarrisonLY <HARRISONYOUNG1485@GMAIL.com>
2015-06-28 06:51:29 -0400

    first heroku push

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
-root 'welcome#index'
-post 'welcome/subscribe' => 'welcome#subscribe'
+ root 'welcome#index'
+ post 'welcome/subscribe' => 'welcome#subscribe'
 
  get "about" => 'welcome#about'
  get "press" => 'welcome#press'
- get "contact" => 'welcome#contact'
 
+ match '/contacts', to: 'contacts#new', via: 'get'
+ resources "contacts", only: [:new, :create]
 
 end
