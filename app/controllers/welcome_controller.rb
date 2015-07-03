57439b0c7b680e8c4742dcfd542156c03e1f65fe
HarrisonLY <HARRISONYOUNG1485@GMAIL.com>
2015-07-03 09:08:33 -0400

    mailchimp and jumbo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,12 @@
 class WelcomeController < ApplicationController
 
 def subscribe(optin = false)
-gb = Gibbon::API.new
+gb = Gibbon::API.new("95a9febb4b2d987fc66419798db9d8d2-us10")
 gb.lists.subscribe({
        id: '49304ea680',
        :email => {:email => params[:email][:address]},
        double_optin: optin,
     })
-end
+ end
 
 end
-
