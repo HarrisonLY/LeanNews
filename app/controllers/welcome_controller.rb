66adfa53241cb285324fae2f66f0adea73d72366
HarrisonLY <HARRISONYOUNG1485@GMAIL.com>
2015-07-10 20:05:25 -0400

    email error skip filter

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+skip_before_action :verify_authenticity_token
 
 def subscribe(optin = false)
 gb = Gibbon::API.new("95a9febb4b2d987fc66419798db9d8d2-us10")
